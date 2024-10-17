122df73ed1a1d77b9a9a1f64d7592d9c3980b0ec
Sunera25 <sandaruwankds.21@uom.lk>
2024-10-18 00:44:35 +0530

    Updated folder structure

---
+++ backend/recipe-service/src/main/java/com/recipesharing/recipe_service/RecipeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recipesharing.recipe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeServiceApplication.class, args);
+	}
+
+}
